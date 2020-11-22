74e3e3c8bfe5b893024815acecb4f25ab6a1a99d
Tim Schindler <xh3b4sd@gmail.com>
2020-11-22 18:13:48 +0100

    add tests (#4)

--- src/search/index.test.ts
+++ src/search/index.test.ts
@@ -42,3 +42,30 @@ test("with digit", () => {
   expect(l).toHaveLength(1);
   expect(l).toContain(666);
 });
+
+test("mixed multiples", () => {
+  let s: string = "THIS IS JUST 10TIMES!!! BETTER";
+
+  let l: number[] = search.Search(s);
+
+  expect(l).toHaveLength(1);
+  expect(l).toContain(10);
+});
+
+test("mixed multiples", () => {
+  let s: string = "my stock went 10x within 6 months";
+
+  let l: number[] = search.Search(s);
+
+  expect(l).toHaveLength(2);
+  expect(l).toContain(10);
+  expect(l).toContain(6);
+});
+
+test("javascript code", () => {
+  let s: string = 'console.log("hello, world!"); // > hello, world! ';
+
+  let l: number[] = search.Search(s);
+
+  expect(l).toHaveLength(0);
+});
